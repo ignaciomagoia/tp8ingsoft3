777fec2663a6bb124f5dc8e2b57f41a5544c2d4b
Ignacio Magoia <2217981@ucc.edu.ar>
2025-11-18 00:40:15 -0300

    hice web apss

--- frontend/public/env-config.js
+++ frontend/public/env-config.js
@@ -1,3 +1,3 @@
-window.__ENV__ = window.__ENV__ || {};
-window.__ENV__.VITE_API_URL = window.__ENV__.VITE_API_URL || "http://localhost:8080";
-window.__ENV__.REACT_APP_API_URL = window.__ENV__.REACT_APP_API_URL || window.__ENV__.VITE_API_URL;
+window.__ENV__ = {
+  VITE_API_URL: "http://localhost:8080"
+};
